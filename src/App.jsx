f5a4bf7179b225143c18375fd3b3617407fbbe89
Nanda <nnvnxx.10@@gmail.com>
2026-02-02 04:17:19 +0700

    maen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
 import { AuthProvider } from './hooks/useAuth';
 import { AlertProvider } from './context/AlertContext';
 import AdminRoute from './components/AdminRoute';
@@ -22,34 +21,32 @@ const PageLoader = () => (
 
 function App() {
   return (
-    <HelmetProvider>
-      <AlertProvider>
-        <AuthProvider>
-          <Router>
-            <Suspense fallback={<PageLoader />}>
-              <Routes>
-                {/* Main App Layout (Protected) */}
-                <Route element={
-                  <AdminRoute>
-                    <AdminLayout />
-                  </AdminRoute>
-                }>
-                  <Route path="/" element={<FrameManager />} />
-                  <Route path="/frames/new" element={<FrameEditor />} />
-                  <Route path="/frames/edit/:id" element={<FrameEditor />} />
-                  <Route path="/links" element={<LinkManager />} />
-                </Route>
+    <AlertProvider>
+      <AuthProvider>
+        <Router>
+          <Suspense fallback={<PageLoader />}>
+            <Routes>
+              {/* Main App Layout (Protected) */}
+              <Route element={
+                <AdminRoute>
+                  <AdminLayout />
+                </AdminRoute>
+              }>
+                <Route path="/" element={<FrameManager />} />
+                <Route path="/frames/new" element={<FrameEditor />} />
+                <Route path="/frames/edit/:id" element={<FrameEditor />} />
+                <Route path="/links" element={<LinkManager />} />
+              </Route>
 
-                <Route path="/login" element={<Login />} />
+              <Route path="/login" element={<Login />} />
 
-                {/* 404 Route */}
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </Suspense>
-          </Router>
-        </AuthProvider>
-      </AlertProvider>
-    </HelmetProvider>
+              {/* 404 Route */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
+        </Router>
+      </AuthProvider>
+    </AlertProvider>
   );
 }
 
